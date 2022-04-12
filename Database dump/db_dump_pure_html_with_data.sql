c22b4cb5f93ee2c11ef8daeb32a57a05f6e0a168
sararesta <sara.resta00@gmail.com>
2022-04-12 13:09:33 +0200

    Added: Data base dump

---
+++ Database dump/db_dump_pure_html_with_data.sql
@@ -0,0 +1,126 @@
+CREATE DATABASE  IF NOT EXISTS `bank` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `bank`;
+-- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: bank
+-- ------------------------------------------------------
+-- Server version	8.0.28
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `bankaccount`
+--
+
+DROP TABLE IF EXISTS `bankaccount`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `bankaccount` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `UserID` int NOT NULL,
+  `Name` varchar(40) COLLATE utf8mb4_general_ci NOT NULL,
+  `Balance` decimal(10,2) NOT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `UID` (`UserID`),
+  CONSTRAINT `UID` FOREIGN KEY (`UserID`) REFERENCES `user` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `bankaccount`
+--
+
+LOCK TABLES `bankaccount` WRITE;
+/*!40000 ALTER TABLE `bankaccount` DISABLE KEYS */;
+INSERT INTO `bankaccount` VALUES (1,1,'myAccount',15000.00),(2,2,'myAccount',15000.00),(3,3,'myAccount',20000.00),(4,4,'default',20000.00),(5,5,'default',25000.00),(6,6,'default',25000.00),(7,1,'accountA',5000.00),(8,1,'AccountB',2000.00),(9,2,'Account1',5000.00),(10,3,'NewAccount',1000.00);
+/*!40000 ALTER TABLE `bankaccount` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `transfer`
+--
+
+DROP TABLE IF EXISTS `transfer`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `transfer` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `Amount` decimal(8,2) NOT NULL,
+  `Timestamp` timestamp NOT NULL,
+  `Reason` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `IDOriginAccount` int NOT NULL,
+  `IDDestinationAccount` int NOT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `OAccount` (`IDOriginAccount`),
+  KEY `DAccount` (`IDDestinationAccount`),
+  CONSTRAINT `DAccount` FOREIGN KEY (`IDDestinationAccount`) REFERENCES `bankaccount` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `OAccount` FOREIGN KEY (`IDOriginAccount`) REFERENCES `bankaccount` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `PositiveAMount` CHECK ((`Amount` >= 0))
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `transfer`
+--
+
+LOCK TABLES `transfer` WRITE;
+/*!40000 ALTER TABLE `transfer` DISABLE KEYS */;
+INSERT INTO `transfer` VALUES (1,40.00,'2020-12-31 23:00:01','bill',1,2),(2,20.00,'2021-01-01 23:00:01','shopping',2,3),(3,500.00,'2021-01-01 23:03:01','new tablet',3,4),(4,1000.00,'2021-03-01 23:05:34','new pc',4,5),(5,10.00,'2021-04-01 22:05:34','grocery',6,1),(6,50.00,'2021-03-01 23:05:34','bill',6,1),(7,30.00,'2021-12-31 23:00:01','shopping',7,1),(8,50.00,'2022-01-01 23:00:01','shopping',8,5),(9,5.00,'2022-01-01 23:03:01','breakfast',10,5);
+/*!40000 ALTER TABLE `transfer` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `Username` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `Password` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `Name` varchar(20) COLLATE utf8mb4_general_ci NOT NULL,
+  `Surname` varchar(30) COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `Username` (`Username`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'sara.resta@mail.polimi.it','pw001','Sara','Resta'),(2,'federico.toschi@mail.polimi.it','pw002','Federico','Toschi'),(3,'alessia.rossi@gmail.com','pw003','Alessia','Rossi'),(4,'giulio.verdi@gmail.com','pw004','Giulio','Verdi'),(5,'gianfilippo.bianchi@libero.it','pw005','Gianfilippo','Bianchi'),(6,'giuseppina.rosa@libero.it','pw006','Giuseppina','Rosa');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping events for database 'bank'
+--
+
+--
+-- Dumping routines for database 'bank'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-04-12 13:02:09
---
+++ Database dump/db_pure_html_without_data.sql
@@ -0,0 +1,66 @@
+CREATE DATABASE  IF NOT EXISTS `bank` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `bank`;
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+-- Table USER
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+	`ID` integer AUTO_INCREMENT,
+	`Username` varchar(50) not null,
+	`Password` varchar(50) not null,
+	`Name` varchar(20) not null,
+	`Surname` varchar(30) not null,
+	primary key(`ID`),
+	unique(`Username`)
+	)engine=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+DROP TABLE IF EXISTS `BankAccount`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `BankAccount`(
+	`ID` integer AUTO_INCREMENT primary key,
+	`UserID` integer not null,
+	`Name` varchar(40) not null,
+	`Balance` decimal(10,2) not null,
+	constraint `UID` foreign key(`UserID`) references `User`(`ID`) on update cascade on delete cascade
+	)engine=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+	
+DROP TABLE IF EXISTS `Transfer`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Transfer`(
+	`ID` integer AUTO_INCREMENT primary key,
+	`Amount` decimal(8,2) not null,
+	`Timestamp` timestamp not null,
+	`Reason` varchar(255) not null,
+	`IDOriginAccount` integer not null,
+	`IDDestinationAccount` integer not null,
+	constraint `OAccount`foreign key(`IDOriginAccount`) references `BankAccount`(`ID`) on update cascade on delete cascade,
+	constraint `DAccount` foreign key(`IDDestinationAccount`) references BankAccount(`ID`) on update cascade on delete cascade,
+	constraint `PositiveAMount` check (`Amount`>=0))engine=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
