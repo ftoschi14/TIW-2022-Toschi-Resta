b4261a7b88629777430e76d87ba8a9a693bb2bb8
sararesta <sara.resta00@gmail.com>
2022-04-25 11:36:03 +0200

    Added servlet Logout.java

---
+++ Code/Banking_System_Pure_Html/src/main/java/it/polimi/tiw/controllers/Logout.java
@@ -0,0 +1,37 @@
+package it.polimi.tiw.controllers;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+@WebServlet("/Logout")
+public class Logout extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    
+    public Logout() {
+        super();
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		HttpSession session = request.getSession(false);
+		if(session != null) {
+			session.invalidate();
+		}
+		String path = getServletContext().getContextPath() + "/login.html";
+		response.sendRedirect(path);
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
