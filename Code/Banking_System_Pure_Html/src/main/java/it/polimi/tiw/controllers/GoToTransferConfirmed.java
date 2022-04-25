64a088be47318c4760742fe50c47b4e0affb9ef0
sararesta <sara.resta00@gmail.com>
2022-04-25 15:03:51 +0200

    Create GoToTransferConfirmed.java

---
+++ Code/Banking_System_Pure_Html/src/main/java/it/polimi/tiw/controllers/GoToTransferConfirmed.java
@@ -0,0 +1,64 @@
+package it.polimi.tiw.controllers;
+
+import java.io.IOException;
+import java.sql.Connection;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
+import it.polimi.tiw.utils.ConnectionHandler;
+
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
+
+@WebServlet("/GoToTransactionConfirmed")
+public class GoToTransferConfirmed extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private TemplateEngine templateEngine;
+	private Connection connection = null;
+    
+    public GoToTransferConfirmed() {
+        super();
+    }
+    
+    public void init() throws ServletException {
+		ServletContext servletContext = getServletContext();
+		ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver(servletContext);
+		templateResolver.setTemplateMode(TemplateMode.HTML);
+		this.templateEngine = new TemplateEngine();
+		this.templateEngine.setTemplateResolver(templateResolver);
+		templateResolver.setSuffix(".html");
+		connection = ConnectionHandler.getConnection(getServletContext());
+	}
+    
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// If the user is not logged in (not present in session) redirect to the login
+		// TO-DO: FILTER TO CHECK IF THE USER IS LOGGED
+		String loginpath = getServletContext().getContextPath() + "/index.html";
+		HttpSession session = request.getSession();
+		if (session.isNew() || session.getAttribute("user") == null) {
+			response.sendRedirect(loginpath);
+			return;
+		}
+		
+		
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
