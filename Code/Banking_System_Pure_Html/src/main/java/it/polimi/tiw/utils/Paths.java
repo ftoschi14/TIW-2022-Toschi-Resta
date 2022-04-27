85098601e26180617d1b2d81c3de52ad98c0f247
sararesta <sara.resta00@gmail.com>
2022-04-27 20:52:14 +0200

    Create Paths.java

---
+++ Code/Banking_System_Pure_Html/src/main/java/it/polimi/tiw/utils/Paths.java
@@ -0,0 +1,22 @@
+package it.polimi.tiw.utils;
+
+public class Paths {
+	
+	public static String pathToLoginPage = "/WEB-INF/Login.html";
+	public static String pathToRegistrationPage = "/WEB-INF/Registration.html";
+	public static String pathToHomePage = "/WEB-INF/Home.html";
+	public static String pathToAccountDetailsPage = "/WEB-INF/AccountDetails.html";
+	public static String pathToTransferConfirmedPage = "/WEB-INF/TransferConfirmed.html";
+	public static String pathToTransferFailedPage = "/WEB-INF/TransferFailed.html";
+	public static String pathToErrorPage = "/WEB-INF/ErrorPage.html";
+	
+	public static String pathToLoginServlet = "/Login";
+	public static String pathToGoToLoginServlet = "/GoToLogin";
+	public static String pathToRegistrationServlet = "/Registration";
+	public static String pathToGoToHomeServlet = "/GoToHome";
+	public static String pathToSelectAccountServlet = "/SelectAccount";
+	public static String pathToGoToTransferConfirmedServlet = "/GoTransferConfirmed";
+	public static String pathToGoToTransferFailedServlet = "/GoTransferFailed";
+	
+
+}
