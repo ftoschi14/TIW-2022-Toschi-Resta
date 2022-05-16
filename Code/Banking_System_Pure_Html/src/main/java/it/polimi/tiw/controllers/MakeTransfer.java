2657ddb9c53df24aeec89d1e417e752f4cc85c79
sararesta <sara.resta00@gmail.com>
2022-05-16 14:59:23 +0200

    Update MakeTransfer.java

--- Code/Banking_System_Pure_Html/src/main/java/it/polimi/tiw/controllers/MakeTransfer.java
+++ Code/Banking_System_Pure_Html/src/main/java/it/polimi/tiw/controllers/MakeTransfer.java
@@ -132,28 +132,28 @@ public class MakeTransfer extends HttpServlet {
 				request.setAttribute("failReason", "The recipient account doesn't exist");
 				request.setAttribute("senderid", senderAccount.getID());
 				path = Paths.pathToTransferFailedPage;
-				forwardToTransferDetails(request, response, path);
+				
 			}
 			else if(recipientAccount.getUserID() != recipientUserID) {
 				//TO-DO REDIRECT TransactionFailed
 				request.setAttribute("failReason", "The user selected is not the owner of the recipient account selected");
 				request.setAttribute("senderid", senderAccount.getID());
 				path = Paths.pathToTransferFailedPage;
-				forwardToTransferDetails(request, response, path);
+
 			}
 			else if(recipientAccount.getID() == senderAccount.getID()) {
 				//TO-DO REDIRECT TransactionFailed
 				request.setAttribute("failReason", "sender account ID = recipient account ID");
 				request.setAttribute("senderid", senderAccount.getID());
 				path = Paths.pathToTransferFailedPage;
-				forwardToTransferDetails(request, response, path);
+
 			}
 			else if(amount.compareTo(senderAccount.getBalance()) == 1) {
 				//TO-DO REDIRECT TransactionFailed
 				request.setAttribute("failReason", "Your account can't afford this transfer");
 				request.setAttribute("senderid", senderAccount.getID());
 				path =  Paths.pathToTransferFailedPage;
-				forwardToTransferDetails(request, response, path);
+
 			}
 			else {
 				TransferDAO transferDAO = new TransferDAO(connection);
@@ -167,8 +167,9 @@ public class MakeTransfer extends HttpServlet {
 				request.setAttribute("newBalanceRecipientAccount", recipientAccount.getBalance().add(amount));
 
 				path = Paths.pathToTransferConfirmedPage;
-				forwardToTransferDetails(request, response, path);
+
 			}
+			forwardToTransferDetails(request, response, path);
 
 		}catch (SQLException e) {
 			errorRedirect(request, response, "Unable to submit this transfer, please try again");
@@ -182,16 +183,16 @@ public class MakeTransfer extends HttpServlet {
 		final WebContext context = new WebContext(request, response, servletContext, request.getLocale());
 		engine.process(path, context, response.getWriter());
 	}
-	
+
 	private void errorRedirect(HttpServletRequest req, HttpServletResponse res, String error) throws IOException {
 		ServletContext servletContext = getServletContext();
 		int senderID = Integer.parseInt(StringEscapeUtils.escapeJava(req.getParameter("senderID")));
 		req.setAttribute("bankAccountID", senderID);
 		req.setAttribute("backPath", Paths.pathToSelectAccountServlet);
 		req.setAttribute("error", error);
-		
+
 		final WebContext context = new WebContext(req, res, servletContext, req.getLocale());
-				
+
 		engine.process(Paths.pathToErrorPage, context, res.getWriter());
 	}
 
