954d43e091825fd39599043e5b0c804ebb842b51
sararesta <sara.resta00@gmail.com>
2022-06-03 23:27:36 +0200

    Create Contact.java

---
+++ Code/Banking_System_RIA/src/main/java/it/polimi/tiw/beans/Contact.java
@@ -0,0 +1,32 @@
+package it.polimi.tiw.beans;
+
+public class Contact{
+
+  private int ownerID;
+  private int contactID;
+
+  /**
+  *Getters
+  **/
+
+  public int getOwnerID(){
+    return ownerID;
+  }
+
+  public int getContactID(){
+    return contactID;
+  }
+
+  /**
+  *Setters
+  **/
+
+  public void setOwnerID(int ownerID){
+    this.ownerID = ownerID;
+  }
+
+  public void setContactID(int contactID){
+    this.contactID = contactID;
+  }
+
+}
