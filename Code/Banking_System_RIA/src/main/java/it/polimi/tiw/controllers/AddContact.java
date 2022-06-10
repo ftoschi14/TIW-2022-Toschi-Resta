c354d586e1d2e1998502fe6bfff041ffc99e5de5
sararesta <sara.resta00@gmail.com>
2022-06-10 15:50:35 +0200

    Added Multipart imports

--- Code/Banking_System_RIA/src/main/java/it/polimi/tiw/controllers/AddContact.java
+++ Code/Banking_System_RIA/src/main/java/it/polimi/tiw/controllers/AddContact.java
@@ -5,6 +5,7 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
--- Code/Banking_System_RIA/src/main/java/it/polimi/tiw/controllers/Register.java
+++ Code/Banking_System_RIA/src/main/java/it/polimi/tiw/controllers/Register.java
@@ -7,6 +7,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
