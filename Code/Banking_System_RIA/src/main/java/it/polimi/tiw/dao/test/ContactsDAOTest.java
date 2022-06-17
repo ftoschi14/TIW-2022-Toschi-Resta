fd7fe0915cbcd47b02a1413b1028bd9386bdeead
Federico Toschi <100305200+ftoschi14@users.noreply.github.com>
2022-06-17 23:21:35 +0200

    Tested ContactDAO

---
+++ Code/Banking_System_RIA/src/main/java/it/polimi/tiw/dao/test/ContactsDAOTest.java
@@ -0,0 +1,51 @@
+package it.polimi.tiw.dao.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import it.polimi.tiw.beans.Contacts;
+import it.polimi.tiw.dao.ContactsDAO;
+
+class ContactsDAOTest {
+	ContactsDAO dao;
+	Connection con;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		con = DriverManager.getConnection("jdbc:mysql://localhost:3306/bank_ria?serverTimezone=UTC",
+				"tiw_admin", "tiw_admin");
+		
+		dao = new ContactsDAO(con);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		con.close();
+	}
+
+	@Test
+	void testFetch() {
+		try {
+			Contacts cts = dao.getContactsByUserID(1);
+			
+			System.out.println("Owner: " + cts.getOwnerID());
+			cts.getContacts().keySet().stream().forEach((entry) -> {
+				System.out.println("AccountOwner: " + entry);
+				cts.getContacts().get(entry).stream().forEach((i) -> {
+					System.out.println(" - Account ID: " + i);
+				});
+			});
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
